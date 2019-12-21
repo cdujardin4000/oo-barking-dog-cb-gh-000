49b649d2149b0bb499f200496db0fe4f41e2d307
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-21 20:38:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,15 @@
 # Your code goes here!
+
 class Dog
-
-  def name=(name)
-    @name = name
-  end
-
-
-  def initialize(name, bark)
-    @name = name
-    @bark = bark
-  end
-
   def bark
     puts "woof!"
   end
 
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
 end
